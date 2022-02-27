85a658645e9b3777ab08e7f09c1632ebe78ad087
edunter <eagr1999@gmail.com>
2022-02-27 16:45:07 +0000

    tweak

--- chat/server/server.js
+++ chat/server/server.js
@@ -1,12 +1,14 @@
 const io = require('socket.io')(process.env.PORT || 3000, {
     cors: {
         origin: '*',
-        methods: ['GET','POST'],
+        methods: ['GET', 'POST'],
         credentials: true
     }
 });
 
 io.on('connection', (socket) => {
     console.log('connected');
-    io.emit('message', 'Hello, World!');
+    socket.on('message', (message) => {
+        io.emit('message', message);
+    });
 });
