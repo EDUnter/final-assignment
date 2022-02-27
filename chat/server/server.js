a36615b17ba27de9432263bfb62715e89fea5298
edunter <eagr1999@gmail.com>
2022-02-27 19:26:33 +0000

    even even more tweaks

--- chat/server/server.js
+++ chat/server/server.js
@@ -6,15 +6,23 @@ const io = require('socket.io')(process.env.PORT || 3000, {
     }
 });
 
+let users = [];
+
 io.on('connection', (socket) => {
     console.log('connected');
     socket.on('message', (message) => {
+        users.forEach(u => {
+            if (u.socketId == socket.id) {
+                message = `${u.nick} ${message}`;
+            }
+        });
         io.emit('message', message);
     });
 
     socket.on('login', (nickname) => {
-        console.log(nickname)
-        socket.emit('signedIn', true);
+        users.push({ nick: nickname, socketId: socket.id });
+        socket.emit('signedIn', { isLoggedIn: true, nick: nickname, socketId: socket.id });
+        socket.broadcast.emit('loggedIn', `${nickname} has joined the chat`);
     });
 
     socket.emit('some event', 'some data');
